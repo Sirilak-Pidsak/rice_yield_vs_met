aaf0dcb88d34cfc930c7d445f3a972526aa6950f
Aditsuda-Jamroensan <aditsuda@gmail.com>
2017-06-11 20:47:35 +0700

    Add files via upload

--- functions.R
+++ functions.R
@@ -2,7 +2,7 @@
 #
 #
 
-# read my data ("my_n" is the end row )
+# read my data 
 read_my_data <- function(my_string, my_sheet, my_n) {
     read_excel(path = my_string,
                sheet = my_sheet, 
@@ -20,8 +20,11 @@ reformat_temp_data <- function(x) {
         gather(Day, Temperature, -Month_Year) %>% # gather all day columns into "Day" and "Temperature" columns
         mutate(Day = str_trim(Day)) %>% # remove character white space from Day column
         mutate(Day = as.numeric(Day)) %>% # convert Day column to numeric from character
-        mutate(Date = ymd(Month_Year) + (Day - 1)) %>% # create Date column from Month_Year and Day
-        select(Date, Temperature) # select columns "Date" and "Temperature" only
+        mutate(Month = month(Month_Year)) %>% # create Month field
+        mutate(Year = year(Month_Year)) %>% # create Year field
+        mutate(Date = as.Date(paste(Year, Month, Day, sep = "-"))) %>% # create Date field
+        filter(!is.na(Date)) %>% # remove all Dates having NA value
+        select(Date, Day, Month, Year, Temperature) # select columns "Date" and "Temperature" only
 }
 
 # reformat humidity data
@@ -32,8 +35,11 @@ reformat_humidity_data <- function(x) {
         gather(Day, Humidity, -Month_Year) %>% # gather all day columns into "Day" and "Temperature" columns
         mutate(Day = str_trim(Day)) %>% # remove character white space from Day column
         mutate(Day = as.numeric(Day)) %>% # convert Day column to numeric from character
-        mutate(Date = ymd(Month_Year) + (Day - 1)) %>% # create Date column from Month_Year and Day
-        select(Date, Humidity) # select columns "Date" and "Temperature" only
+        mutate(Month = month(Month_Year)) %>% # create Month field
+        mutate(Year = year(Month_Year)) %>% # create Year field
+        mutate(Date = as.Date(paste(Year, Month, Day, sep = "-"))) %>% # create Date field
+        filter(!is.na(Date)) %>% # remove all Dates having NA value
+        select(Date, Humidity) # select columns "Date" and "Humidity" only
 }
 
 
@@ -45,19 +51,9 @@ reformat_precip_data <- function(x) {
         gather(Day, Precipitation, -Month_Year) %>% # gather all day columns into "Day" and "Temperature" columns
         mutate(Day = str_trim(Day)) %>% # remove character white space from Day column
         mutate(Day = as.numeric(Day)) %>% # convert Day column to numeric from character
-        mutate(Date = ymd(Month_Year) + (Day - 1)) %>% # create Date column from Month_Year and Day
-        select(Date, Precipitation) # select columns "Date" and "Temperature" only
-}
-
-
-# reformat yield data
-reformat_precip_data <- function(x) {
-    x %>%
-        rename(Month_Year = `Month/Year`) %>% # rename "Month/Year" to "Month_Year"
-        select(-No, -Station, -Total) %>% # remove column "No", "Station" and "X__1" from tbl_temp
-        gather(Day, Precipitation, -Month_Year) %>% # gather all day columns into "Day" and "Temperature" columns
-        mutate(Day = str_trim(Day)) %>% # remove character white space from Day column
-        mutate(Day = as.numeric(Day)) %>% # convert Day column to numeric from character
-        mutate(Date = ymd(Month_Year) + (Day - 1)) %>% # create Date column from Month_Year and Day
-        select(Date, Precipitation) # select columns "Date" and "Temperature" only
+        mutate(Month = month(Month_Year)) %>% # create Month field
+        mutate(Year = year(Month_Year)) %>% # create Year field
+        mutate(Date = as.Date(paste(Year, Month, Day, sep = "-"))) %>% # create Date field
+        filter(!is.na(Date)) %>% # remove all Dates having NA value
+        select(Date, Precipitation) # select columns "Date" and "Precipitation" only
 }
